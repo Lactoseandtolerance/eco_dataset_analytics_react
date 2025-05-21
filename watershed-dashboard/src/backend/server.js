93654cf06b1ef17e410c8b29a22176132515765d
Darling <99622500+DataDarling@users.noreply.github.com>
2025-05-21 14:16:00 -0400

    Update server.js

--- watershed-dashboard/src/backend/server.js
+++ watershed-dashboard/src/backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Wellness1919',
+  password: '*******',
   database: 'water_quality_tracker',
 });
 
